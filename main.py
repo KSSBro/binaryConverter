755f2d0b13b03f211c5360028f4e8b5da46bcb3c
KSSBro <shashwatdeadcliff@gmail.com>
2020-01-03 23:49:24 +0530

    added options

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ def cli():
     """Made by KSSBro"""
 
 @click.command()
-def convert():
+@click.option('-from-type', prompt="From", help="The system you want to convert from")
+@click.option('-to-type', prompt="To", help="The system you want to convert to")
+@click.option('-value', prompt="Value", help="The value you want to convert")
+@click.option('-create-file', default="false", help="true/false")
+@click.option('--file-type', default="json", help="text/json")
+def convert(from_type, to_type, value, create_file, file_type):
+    print(from_type, to_type)
     print("Convert function.")
 
 cli.add_command(convert)
