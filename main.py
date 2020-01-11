15888fa31af2a3b42877c155753743e8f5ea60f0
KSSBro <shashwatdeadcliff@gmail.com>
2020-01-11 22:41:37 +0530

    changes

--- main.py
+++ main.py
@@ -6,34 +6,23 @@ def cli():
     """Made by KSSBro"""
 
 @click.command()
+@click.option('-type', promt="Type", help="encode | decode")
 @click.option('-form', prompt="Format", help="decimal | binary | octal | hexadecimal | base64")
 @click.option('-text', prompt="Text", help="The text you want to encode")
 @click.option('-delimiter', default="space", help="space(default) | userd")
 @click.option('--delim-ud', default=" ", help="The delimiter you want to use")
-def encode(form, text, delimiter, delim_ud):
+def codec(type_, form, text, delimiter, delim_ud):
     if delimiter != "space":
         delim = delim_ud
     else:
         delim = " "
 
-    controller.controller(form, text, delim, "encode")
+    result = controller.controller(form, text, delim, type_)
 
 @click.command()
-@click.option('-form', prompt="Format", help="decimal | binary | octal | hexadecimal | base64")
-@click.option('-text', prompt="Text", help="The encoded text you want to decode")
-@click.option('-delimiter', default="space", help="space(default) | userd")
-@click.option('--delim-ud', default=" ", help="The delimiter of the encoded text.")
-def decode(form, text, delimiter, delim_ud):
-    if delimiter != "space":
-        delim = delim_ud
-    else:
-        delim = " "
-    
-    controller.controller(form, text, delim, "decode")
 
 
-cli.add_command(encode)
-cli.add_command(decode)
+cli.add_command(codec)
 
 if __name__ == "__main__":
     cli()
